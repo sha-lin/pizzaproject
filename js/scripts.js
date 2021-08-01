d602a18e2f769003aaf129a2181cc425e554262c
shalin <shalin.rono@student.moringaschool.com>
2021-08-01 12:13:07 +0300

    remove name variable in contact us part

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,6 @@ if (location !== "") {
 $(document).ready(function(){
     $("form#form34A").submit(function(event){
       // event.preventDefault();
-      var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
       if ($("input#MERGE1").val() && $("input#MERGE0").val()){
